8f175d2842a1ff18b272342b341018c759a9c610
Matthijs Breemans <matthijs@groov.nl>
2016-01-14 19:54:53 +0100

    day19

--- AdventOfCode15Day19/src/AdventOfCode15Day19/Program.cs
+++ AdventOfCode15Day19/src/AdventOfCode15Day19/Program.cs
@@ -11,11 +11,21 @@ namespace AdventOfCode15Day19
     {
         public void Main(string[] args)
         {
+            var result = new List<string>();
+            var all = File.ReadAllLines("input.txt");
+            var input = all.Take(all.Length - 1);
+            
+            foreach (var line in input)
+            {
+                var key = line.Replace(" =>", "").Split(' ')[0];
+                var val = line.Replace(" =>", "").Split(' ')[1];
 
-
+                result.AddRange(from Match m in Regex.Matches(all.Last(), key) select all.Last().Remove(m.Index, key.Length).Insert(m.Index, val));
+            }
+            var x = result.Distinct().Count();
         }
-
 
     }
 
 }
+
